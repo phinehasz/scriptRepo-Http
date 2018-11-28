29cd95162f0ebe865941fcde44416548013572ea
phinehasz <zhhiyp@qq.com>
2018-11-29 00:03:11 +0800

    for_learn2

--- python/for_test.py
+++ python/for_test.py
@@ -2,17 +2,21 @@
 
 list = [-1, 3, 4]
 
+
 def findMinAndMax(list):
-    if(len(list)==0):
-        return None,None
+    if (len(list) == 0):
+        return None, None
     min = list[0]
     max = min
     for x in list:
-        if x>=max:
+        if x >= max:
             max = x
-        if x<= min:
+        if x <= min:
             min = x
     return (min, max)
 
 
 print(findMinAndMax(list))
+
+print([x * x for x in range(1, 11) if x % 2 == 0])
+print([x * y for x in 'zxy' for y in range(3,7)])
